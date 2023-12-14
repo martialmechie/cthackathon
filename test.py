03ef45d6ee6cfd9bc07970beaa1daf725af5b6f7
Abhishek Ghosh <abhishekghsh8@gmail.com>
2023-12-14 11:41:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
 
+import os
+
+import openai
+
+openai.api_type = "azure"
+
+openai.api_base = "https://dummy-openai-resource.openai.azure.com/"
+
+openai.api_version = "2023-07-01-preview"
+
+openai.api_key = os.getenv("")
+
+message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":"Hi"},{"role":"assistant","content":"Hello! How can I assist you today?"}]
+
+
+completion = openai.ChatCompletion.create(
+
+engine="dummy-gpt-35-turbo",
+
+messages = message_text,
+
+temperature=0.7,
+
+max_tokens=800,
+
+top_p=0.95,
+
+frequency_penalty=0,
+
+presence_penalty=0,
+
+stop=None
+
+)
+
